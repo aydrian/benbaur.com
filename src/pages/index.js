a9e2366b18f48edf55f5034d15c1842d4c9095df
Aydrian J. Howard <aydrian@gmail.com>
2017-09-04 14:35:41 -0400

    Added social media links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import {Icon} from 'react-fa';
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <ul>
+      <li><a href="https://www.facebook.com/Ben-Baur-363420870338396/"><Icon name="facebook" /> Facebook</a></li>
+      <li><a href="https://twitter.com/benjbaur"><Icon name="twitter" /> Twitter</a></li>
+      <li><a href="https://www.instagram.com/benjbaur/"><Icon name="instagram" /> Instagram</a></li>
+      <li><a href="http://www.imdb.com/name/nm3553654/"><Icon name="imdb" /> IMDb</a></li>
+    </ul>
   </div>
 )
 
